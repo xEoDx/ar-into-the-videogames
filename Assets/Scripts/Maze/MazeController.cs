820043d3c686dd951912aff6f6babf1a84faae56
xEoDx <victor.alcazar.alcazar@gmail.com>
2020-02-03 18:30:50 +0100

    Initial commit with maze prototype

---
+++ Assets/Scripts/Maze/MazeController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Maze
+{
+    public class MazeController : MonoBehaviour
+    {
+        public GameObject ballPrefab;
+
+        public Transform spawnPosition;
+
+        private GameObject _instantiatedBall;
+
+        public void StartMaze()
+        {
+            _instantiatedBall = Instantiate(ballPrefab, spawnPosition.position, Quaternion.identity, spawnPosition.parent);
+        }
+
+        public void StopMaze()
+        {
+            Destroy(_instantiatedBall);
+        }
+    }
+}
